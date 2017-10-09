72a4705ee03aaa4d5ff9a2cc2183f29606c6b1e8
Gavin Greer <gavingreer@mac.com>
2017-10-09 16:27:47 -0500

    Files as of Lesson 35

--- src/tweetme/settings/local.py
+++ src/tweetme/settings/local.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'rest_framework',
+    'accounts',
     'tweets'
 ]
 
--- src/tweetme/settings/production.py
+++ src/tweetme/settings/production.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'rest_framework',
+    'accounts',
     'tweets'
 ]
 
