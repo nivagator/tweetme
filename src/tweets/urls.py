629912eb5aa60bafc0a2075a4201e4a6f6336259
Gavin Greer <gavingreer@mac.com>
2017-10-09 14:22:56 -0500

    Files as of Lesson 30

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from .views import (
     TweetCreateView,
     TweetDeleteView,
@@ -8,7 +9,8 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^search/$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>[0-9]+)/$', TweetDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/update/$', TweetUpdateView.as_view(), name='update'),
