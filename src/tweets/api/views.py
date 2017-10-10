91989530c0e0999238887f18bbeeb70013edc959
Gavin Greer <gavingreer@mac.com>
2017-10-09 22:50:33 -0500

    Files as of Lesson 37

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
@@ -7,5 +8,13 @@ class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     # permission_classes = (IsAdminUser,)
     
-    def get_queryset(self):
-        return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query) 
+                )
+        return qs
