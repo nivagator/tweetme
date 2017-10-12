01f12eaf5e9907cd290c7b485670385161119ff8
Gavin Greer <gavingreer@mac.com>
2017-10-12 14:20:08 -0500

    Files as of Lesson 50

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -14,8 +14,24 @@ class UserProfileManager(models.Manager):
         except:
             pass
         return qs
+    
+    def toggle_follow(self, user, to_toggle_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user) # returns (user_obj, true)
+        if to_toggle_user in user_profile.following.all():
+            user_profile.following.remove(to_toggle_user)
+            added = False
+        else:
+            user_profile.following.add(to_toggle_user)
+            added = True
+        return added
 
-
+    def is_following(self, user, followed_by_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user) # returns (user_obj, true)
+        if created:
+            return False
+        if followed_by_user in user_profile.following.all():
+            return True
+        return False
 
 class UserProfile(models.Model):
     user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') # user.profile ... 
