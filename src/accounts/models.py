439319914405bcacfd8a941e6f5a82d5d00d7e85
Gavin Greer <gavingreer@mac.com>
2017-10-12 10:07:01 -0500

    Files as of Lesson 47

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') # user.profile ... 
+    following   = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by') 
+    # user.profile.following -- users i follow
+    # user.followed_by -- users that follow me
+    
+    def __str__(self):
+        return str(self.following.all().count())
