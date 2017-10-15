60132bc3d15397532df4fda366fdef455fedd285
Gavin Greer <gavingreer@mac.com>
2017-10-15 16:48:51 -0500

    Files as of Lesson 61

--- src/hashtags/models.py
+++ src/hashtags/models.py
@@ -3,6 +3,8 @@ from django.urls import reverse_lazy
 from tweets.models import Tweet
 # Create your models here.
 
+from .signals import parsed_hashtags
+
 class HashTag(models.Model):
     tag = models.CharField(max_length=120)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -15,3 +17,12 @@ class HashTag(models.Model):
     
     def get_tweets(self):
         return Tweet.objects.filter(content__icontains="#" + self.tag)
+
+
+
+def parsed_hashtags_receiver(sender, hashtag_list, *args, **kwargs):
+    if len(hashtag_list) > 0:
+        for tag_var in hashtag_list:
+            new_tag, create = HashTag.objects.get_or_create(tag=tag_var)
+
+parsed_hashtags.connect(parsed_hashtags_receiver)
