036a793737bcc75351986055b27586bfbf482a35
Gavin Greer <gavingreer@mac.com>
2017-10-17 23:54:10 -0500

    Files as of Lesson 74

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
-from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+
 from django.views import View
+from django.views.generic import DetailView
+# Create your views here.
+
 from .models import UserProfile
 
-# Create your views here.
 User = get_user_model()
 
 class UserDetailView(DetailView):
@@ -18,6 +21,7 @@ class UserDetailView(DetailView):
         context = super(UserDetailView, self).get_context_data(*args, **kwargs)
         following = UserProfile.objects.is_following(self.request.user, self.get_object())
         context['following'] = following
+        context['recommended'] = UserProfile.objects.recommended(self.request.user)
         return context
 
 class UserFollowView(View):
