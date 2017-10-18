69affee8cbbcbb6f90e908adfb98b3fa109f8630
Gavin Greer <gavingreer@mac.com>
2017-10-18 00:47:59 -0500

    Files as of Lesson 76

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -36,7 +36,7 @@ class TweetCreateView(FormUserNeededMixin, CreateView):
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
 
-class TweetListView(ListView):
+class TweetListView(LoginRequiredMixin, ListView):
     
     def get_queryset(self, *args, **kwargs):
         qs = Tweet.objects.all()
